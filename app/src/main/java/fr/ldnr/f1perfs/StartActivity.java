90af56b9d0d6d238151675e21b3342dd65d085cd
Kevin-Diez <kevin.diez.pro@gmail.com>
2017-04-19 15:06:57 +0200

    V 0.1 Créations des menus.xml

--- app/src/main/java/fr/ldnr/f1perfs/StartActivity.java
+++ app/src/main/java/fr/ldnr/f1perfs/StartActivity.java
@@ -1,7 +1,10 @@
 package fr.ldnr.f1perfs;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -10,4 +13,15 @@ public class StartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.start, menu);
+        return true;
+    }
+
+    private void createDatabase() {
+        
+    }
+
 }

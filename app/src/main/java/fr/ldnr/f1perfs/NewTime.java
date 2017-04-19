39f80e540cf6087b68a4bfe70468e0df3830bc37
Kevin-Diez <kevin.diez.pro@gmail.com>
2017-04-19 16:24:39 +0200

    V 0.1 Back to the future

--- app/src/main/java/fr/ldnr/f1perfs/NewTime.java
+++ app/src/main/java/fr/ldnr/f1perfs/NewTime.java
@@ -1,8 +1,10 @@
 package fr.ldnr.f1perfs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class NewTime extends AppCompatActivity {
@@ -17,9 +19,32 @@ public class NewTime extends AppCompatActivity {
     public void saveNewTime(View view) {
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.newtime, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+
+            case R.id.menu_record:
+                startActivity(new Intent(this, NewTime.class));
+                return true;
+
+            /*case R.id.menu_search:
+                startActivity(new Intent(this, SearchActivity.class));
+                return true;*/
+
+            case R.id.menu_backHome:
+                startActivity(new Intent(this, StartActivity.class));
+                return true;
+
+            default:
+                return false;
+        }
+
     }
 }
--- app/src/main/java/fr/ldnr/f1perfs/StartActivity.java
+++ app/src/main/java/fr/ldnr/f1perfs/StartActivity.java
@@ -1,7 +1,11 @@
 package fr.ldnr.f1perfs;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -10,4 +14,33 @@ public class StartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.start, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+
+            case R.id.menu_record:
+                startActivity(new Intent(this, NewTime.class));
+                return true;
+
+            /*case R.id.menu_search:
+                startActivity(new Intent(this, SearchActivity.class));
+                return true;*/
+
+            case R.id.menu_backHome:
+                startActivity(new Intent(this, StartActivity.class));
+                return true;
+
+            default:
+                return false;
+        }
+
+    }
+
 }

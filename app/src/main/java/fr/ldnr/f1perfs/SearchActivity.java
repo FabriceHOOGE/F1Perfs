6c9b5b0452a2d260062b68d1e73fb4b80dc4624c
DavidLdnr <davidalexandre.fournier.pro@gmail.com>
2017-04-19 16:32:08 +0200

    ajout fichier david sur kevin

---
+++ app/src/main/java/fr/ldnr/f1perfs/SearchActivity.java
@@ -0,0 +1,18 @@
+package fr.ldnr.f1perfs;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by dfour on 19/04/2017.
+ */
+
+public class SearchActivity extends Activity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}

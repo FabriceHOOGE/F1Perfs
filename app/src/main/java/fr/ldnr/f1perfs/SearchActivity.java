cb043a5bcdbedeee01f25045bd98b0b32b54dca0
DavidLdnr <davidalexandre.fournier.pro@gmail.com>
2017-04-20 14:54:00 +0200

    mis a jour searchactivity

--- app/src/main/java/fr/ldnr/f1perfs/SearchActivity.java
+++ app/src/main/java/fr/ldnr/f1perfs/SearchActivity.java
@@ -3,10 +3,15 @@ package fr.ldnr.f1perfs;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by dfour on 19/04/2017.
@@ -15,11 +20,26 @@ import android.view.MenuItem;
 public class SearchActivity extends AppCompatActivity{
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
     }
 
+    //methode à utiliser pour la recherche en base de données
+    public void searchActivity(View View) {
+        AutoCompleteTextView actvSearchEvent = (AutoCompleteTextView) findViewById(R.id.search_search_event);
+        TextView tvSearchTimeM = (TextView) findViewById(R.id.search_search_timeM);
+        EditText etSearchMinute = (EditText) findViewById(R.id.search_search_minute);
+        TextView tvSearchTimeS = (TextView) findViewById(R.id.search_search_timeS);
+        EditText etSearchSeconds = (EditText) findViewById(R.id.search_search_seconds);
+        AutoCompleteTextView actvSearchPilot = (AutoCompleteTextView) findViewById(R.id.search_searchpilot);
+        AutoCompleteTextView actvSearchTrack = (AutoCompleteTextView) findViewById(R.id.search_searchtrack);
+        Button btSearchSearch = (Button) findViewById(R.id.search_search);
+
+        Log.i("SearchActivity", "Evenement: "+ actvSearchEvent.getText()+ "|| Temps: "+ tvSearchTimeM.getText()+"||Minute: "+etSearchMinute.getText()+"||" +
+                "|| Temps: "+tvSearchTimeS.getText()+"|| Secondes: "+etSearchSeconds.getText()+"|| Pilote: "+actvSearchPilot.getText()+"|| Circuit: "+actvSearchTrack.getText());
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {

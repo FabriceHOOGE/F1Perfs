23f1f547519e9c99284cef9fe97a3ed752792ae5
Kevin-Diez <kevin.diez.pro@gmail.com>
2017-04-21 16:15:17 +0200

    V 0.6 Graph en cours de chantier

--- app/src/main/java/fr/ldnr/f1perfs/ChartActivity.java
+++ app/src/main/java/fr/ldnr/f1perfs/ChartActivity.java
@@ -1,12 +1,15 @@
 package fr.ldnr.f1perfs;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutCompat;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,6 +19,13 @@ import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.helper.GraphViewXML;
+import com.jjoe64.graphview.helper.StaticLabelsFormatter;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.ArrayList;
 
@@ -25,6 +35,7 @@ public class ChartActivity extends AppCompatActivity {
 
     DBOpenHelper dbOpenHelper;
     LinearLayout layout = null;
+    String[] pilots;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -39,20 +50,90 @@ public class ChartActivity extends AppCompatActivity {
 
         fillTableResult();
 
+        generateChart();
 
-        float[] values = new float[] { 2.0f,1.5f, 2.5f, 1.0f , 3.0f };
-        String[] verlabels = new String[] { "great", "ok", "bad" };
-        String[] horlabels = new String[] { "today", "tomorrow", "next week", "next month" };
-        ChartView chartView = new ChartView(this, values, "GraphViewDemo",horlabels, verlabels, ChartView.LINE);
-
-        ConstraintLayout cl = (ConstraintLayout) findViewById(R.id.chart_layout);
-
-        setContentView(chartView);
-
-
-        //setContentView(layout);
+        setContentView(layout);
 
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        String event = data.getStringExtra("event");
+        String pilot = data.getStringExtra("pilot");
+        String track = data.getStringExtra("track");
+
+        int min = data.getIntExtra("minutemin",0);
+        int max = data.getIntExtra("minutemax",0);
+
+        Log.i("TEST","\nEvent : " + event + "\nPilot : " + pilot + "\nTrack : " + track + "\nMin : " + min + "\nMax : " + max);
+
+    }
+
+    /**
+     *   Génération graphique
+     *
+     *
+     **/
+    public void generateChart()
+    {
+        // Récupération graph
+        GraphView graph = (GraphView) layout.findViewById(R.id.graph);
+
+        // Stylisation graph
+        graph.setTitleColor(ContextCompat.getColor(this, R.color.textColor));
+        graph.getGridLabelRenderer().setGridColor(ContextCompat.getColor(this, R.color.textColor));
+        graph.getGridLabelRenderer().setVerticalLabelsColor(ContextCompat.getColor(this, R.color.textColor));
+        graph.getGridLabelRenderer().setHorizontalLabelsColor(ContextCompat.getColor(this, R.color.textColor));
+        graph.getGridLabelRenderer().setHorizontalAxisTitleColor(ContextCompat.getColor(this, R.color.textColor));
+        graph.getGridLabelRenderer().setVerticalAxisTitleColor(ContextCompat.getColor(this, R.color.textColor));
+        graph.getGridLabelRenderer().setVerticalAxisTitle(getResources().getString(R.string.chart_vTitle));
+
+        // Valeurs axe vertical
+        graph.getViewport().setMinY(0);
+        graph.getViewport().setMaxY(10);
+        graph.getViewport().setYAxisBoundsManual(true);
+
+        // Valeurs axe horizontal (noms pilotes)
+        StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
+        staticLabelsFormatter.setHorizontalLabels(pilots);
+        graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
+
+        // Courbe 1 TEST
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 100),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        series.setTitle("Track 1");
+        graph.addSeries(series);
+
+        // Courbe 2 TEST
+        LineGraphSeries<DataPoint> series2 = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 5),
+                new DataPoint(1, 2),
+                new DataPoint(2, 3),
+                new DataPoint(3, 9),
+                new DataPoint(4, 0)
+        });
+        series2.setTitle("Track 2");
+        // Couleur courbe 2
+        series2.setColor(Color.RED);
+
+        // GetSecondScale apparemment obligatoire
+        graph.getSecondScale().addSeries(series2);
+        // Obligé de rentrer manuellement les valeurs axe vertical 2
+        graph.getSecondScale().setMinY(0);
+        graph.getSecondScale().setMaxY(10);
+        // Couleur axe vertical 2
+        graph.getGridLabelRenderer().setVerticalLabelsSecondScaleColor(Color.RED);
+    }
+
+
+
 
     /**
     *   Génération cellule du tableau
@@ -86,6 +167,14 @@ public class ChartActivity extends AppCompatActivity {
 
         if(dbOpenHelper.isCorrect(queryResult))
         {
+            // Récupération noms des pilotes pour le graph
+            ArrayList<String> tmpArr = new ArrayList<>();
+            for(int i = 0;i < queryResult.get(3).size();i++)
+                if(!tmpArr.contains(queryResult.get(3).get(i)))
+                    tmpArr.add(queryResult.get(3).get(i));
+
+            pilots = tmpArr.toArray(new String[0]);
+
             for(int i = 0;i < queryResult.get(0).size();i++)
             {
                 TableRow row = new TableRow(this);
@@ -116,8 +205,8 @@ public class ChartActivity extends AppCompatActivity {
         String[] select = new String[]{"event", "time", "track", "pilot"}; // SELECT pilot, time
         String[] where = new String[]{}; // Pas de WHERE
         String[] values = new String[]{}; // Pas de WHERE
-        String orderBy = null; // Pas d'ORDER BY
-        String limit = null; // Pas de LIMIT
+        String orderBy = "time"; // ORDER BY time
+        String limit = "6"; // LIMIT 6
         Boolean distinct = false; // Pas de DISTINCT
 
         // Fonction qui envoie le SELECT et retourne un tableau
@@ -146,9 +235,9 @@ public class ChartActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()) {
 
-            /*case R.id.menu_record:
+            case R.id.menu_record:
                 startActivity(new Intent(this, NewTimeActivity.class));
-                return true;*/
+                return true;
 
             case R.id.menu_search:
                 startActivity(new Intent(this, SearchActivity.class));
--- app/src/main/java/fr/ldnr/f1perfs/ChartView.java
+++ app/src/main/java/fr/ldnr/f1perfs/ChartView.java
@@ -5,6 +5,8 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Paint.Align;
+import android.graphics.RectF;
+import android.util.AttributeSet;
 import android.view.View;
 
 /**
@@ -14,118 +16,58 @@ import android.view.View;
 public class ChartView extends View
 {
 
-    public static boolean BAR = true;
-    public static boolean LINE = false;
+    private RectF rect = new RectF();
+    private Paint paint = new Paint();
+    private int percentage;
 
-    private Paint paint;
-    private float[] values;
-    private String[] horlabels;
-    private String[] verlabels;
-    private String title;
-    private boolean type;
+    public ChartView(Context context)
+    {
+        this(context, null);
+    }
 
-    public ChartView(Context context, float[] values, String title, String[] horlabels, String[] verlabels, boolean type) {
-        super(context);
-        if (values == null)
-            values = new float[0];
-        else
-            this.values = values;
-        if (title == null)
-            title = "";
-        else
-            this.title = title;
-        if (horlabels == null)
-            this.horlabels = new String[0];
-        else
-            this.horlabels = horlabels;
-        if (verlabels == null)
-            this.verlabels = new String[0];
-        else
-            this.verlabels = verlabels;
-        this.type = type;
-        paint = new Paint();
+    public ChartView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+    }
+
+    public void setPercentage(int percentage)
+    {
+        this.percentage = percentage;
+        invalidate();
+    }
+
+    public int getPercentage()
+    {
+        return percentage;
     }
 
     @Override
-    protected void onDraw(Canvas canvas) {
-        float border = 20;
-        float horstart = border * 2;
-        float height = getHeight();
-        float width = getWidth() - 1;
-        float max = getMax();
-        float min = getMin();
-        float diff = max - min;
-        float graphheight = height - (2 * border);
-        float graphwidth = width - (2 * border);
-
-        paint.setTextAlign(Align.LEFT);
-        int vers = verlabels.length - 1;
-        for (int i = 0; i < verlabels.length; i++) {
-            paint.setColor(Color.DKGRAY);
-            float y = ((graphheight / vers) * i) + border;
-            canvas.drawLine(horstart, y, width, y, paint);
-            paint.setColor(Color.WHITE);
-            canvas.drawText(verlabels[i], 0, y, paint);
+    protected void onSizeChanged(int w, int h, int oldw, int oldh)
+    {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if (w > h)
+        {
+            rect.set(w / 2 - h / 2, 0, w / 2 + h / 2, h);
         }
-        int hors = horlabels.length - 1;
-        for (int i = 0; i < horlabels.length; i++) {
-            paint.setColor(Color.DKGRAY);
-            float x = ((graphwidth / hors) * i) + horstart;
-            canvas.drawLine(x, height - border, x, border, paint);
-            paint.setTextAlign(Align.CENTER);
-            if (i==horlabels.length-1)
-                paint.setTextAlign(Align.RIGHT);
-            if (i==0)
-                paint.setTextAlign(Align.LEFT);
-            paint.setColor(Color.WHITE);
-            canvas.drawText(horlabels[i], x, height - 4, paint);
-        }
-
-        paint.setTextAlign(Align.CENTER);
-        canvas.drawText(title, (graphwidth / 2) + horstart, border - 4, paint);
-
-        if (max != min) {
-            paint.setColor(Color.LTGRAY);
-            if (type == BAR) {
-                float datalength = values.length;
-                float colwidth = (width - (2 * border)) / datalength;
-                for (int i = 0; i < values.length; i++) {
-                    float val = values[i] - min;
-                    float rat = val / diff;
-                    float h = graphheight * rat;
-                    canvas.drawRect((i * colwidth) + horstart, (border - h) + graphheight, ((i * colwidth) + horstart) + (colwidth - 1), height - (border - 1), paint);
-                }
-            } else {
-                float datalength = values.length;
-                float colwidth = (width - (2 * border)) / datalength;
-                float halfcol = colwidth / 2;
-                float lasth = 0;
-                for (int i = 0; i < values.length; i++) {
-                    float val = values[i] - min;
-                    float rat = val / diff;
-                    float h = graphheight * rat;
-                    if (i > 0)
-                        canvas.drawLine(((i - 1) * colwidth) + (horstart + 1) + halfcol, (border - lasth) + graphheight, (i * colwidth) + (horstart + 1) + halfcol, (border - h) + graphheight, paint);
-                    lasth = h;
-                }
-            }
+        else
+        {
+            rect.set(0, h / 2 - w / 2, w, h / 2 + w / 2);
         }
     }
 
-    private float getMax() {
-        float largest = Integer.MIN_VALUE;
-        for (int i = 0; i < values.length; i++)
-            if (values[i] > largest)
-                largest = values[i];
-        return largest;
-    }
+    @Override
+    protected void onDraw(Canvas canvas)
+    {
+        super.onDraw(canvas);
 
-    private float getMin() {
-        float smallest = Integer.MAX_VALUE;
-        for (int i = 0; i < values.length; i++)
-            if (values[i] < smallest)
-                smallest = values[i];
-        return smallest;
+        canvas.drawColor(Color.WHITE);
+
+        paint.setColor(Color.RED);
+        canvas.drawArc(rect, 0, 360 * percentage / 100, true, paint);
+
+        paint.setColor(Color.BLUE);
+        canvas.drawArc(rect, 360 * percentage / 100, 360 - 360 * percentage / 100, true, paint);
     }
 
 }
